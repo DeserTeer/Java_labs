6ce5f32a763ba5e3b468163debd3975b2cffaac5
unknown <grintsevichf@gmail.com>
2023-12-17 00:32:56 +0300

    Laba5

---
+++ src/bsu/rfe/java/group_5/lab_5/Grintsevich/B_1/Main.java
@@ -0,0 +1,29 @@
+package bsu.rfe.java.group_5.lab_5.Grintsevich.B_1;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+// Создать и показать экземпляр главного окна приложения
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+
+    }
+
+}
+/*String sample = "02468";
+        String Y = ((Double) y).toString();
+        boolean flag = true;
+        for (int i = 0; i < Y.length(); i++) {
+            if (sample.equals(Y.substring(i, i + 1))) {
+                flag = false;
+            }
+        }
+
+        if (flag) {
+            canvas.setColor(Color.green);
+        } else {
+            canvas.setColor(Color.RED);
+        }*/
